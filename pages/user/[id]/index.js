a9a02d4d1bdc9797bbfae1c84c6ac945eb9cfac9
duongsaikik <dinhhaiduongsoma@gmail.com>
2022-03-18 23:50:42 +0700

    Update index.js

--- pages/user/[id]/index.js
+++ pages/user/[id]/index.js
@@ -43,7 +43,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async (context) => {
   const id = context.params.id;
-  const res = await fetch("http://localhost:5035/users/" + id);
+  const res = await fetch("https://shopbug.herokuapp.com/users/" + id);
   const data = await res.json();
   return {
     props: {
